2844e790c63db3736d57497d17c3090b5a1e9794
Stanislav Khomyakov <sikhomyakov@gmail..com>
2022-04-27 00:44:40 +0500

    version 1.0

--- src/main/kotlin/ru/netology/main.kt
+++ src/main/kotlin/ru/netology/main.kt
@@ -3,7 +3,7 @@ package ru.netology
 fun main() {
     val cardType = "Vk Pay"
     val amountTransfersThisMonth = 0
-    val amountTransfer = 1000
+    val amountTransfer = 100_000
 
     limitsCheck(cardType, amountTransfersThisMonth, amountTransfer)
 }
@@ -11,7 +11,7 @@ fun main() {
 fun commissionCalculate(cardType: String, amountTransfersThisMonth: Int, amountTransfer: Int): Int =
     when (cardType) {
         "Maestro", "MasterCard" -> {
-            if (amountTransfersThisMonth <= 75000) 0 else ((amountTransfer * 0.006 + 20) * 100).toInt()
+            if (amountTransfersThisMonth <= 75_000) 0 else ((amountTransfer * 0.006 + 20) * 100).toInt()
         }
         "Visa", "Мир" -> {
             if (amountTransfer * 0.0075 > 35) ((amountTransfer * 0.0075) * 100).toInt() else 35 * 100
@@ -20,9 +20,9 @@ fun commissionCalculate(cardType: String, amountTransfersThisMonth: Int, amountT
     }
 
 fun limitsCheck(cardType: String, amountTransfersThisMonth: Int, amountTransfer: Int) {
-    if (cardType == "Vk Pay" && (amountTransfersThisMonth > 40000 || amountTransfer > 15000)) {
+    if (cardType == "Vk Pay" && (amountTransfersThisMonth > 40_000 || amountTransfer > 15_000)) {
         println("Превышен лимит переводов")
-    } else if (amountTransfersThisMonth < 600000 && amountTransfer < 150000) {
+    } else if (amountTransfersThisMonth < 600_000 && amountTransfer < 150_000) {
         val commission = commissionCalculate(cardType, amountTransfersThisMonth, amountTransfer)
         println("Ваша комиссия составляет ${commission / 100} руб. ${commission % 100} коп.")
     } else {
---
+++ src/test/kotlin/ru/netology/MainKtTest.kt
@@ -0,0 +1,46 @@
+package ru.netology
+
+import org.junit.Test
+
+import org.junit.Assert.*
+import org.junit.Ignore
+
+class MainKtTest {
+
+    @Test
+    fun commissionCalculateMaestro() {
+        //arrage
+        val cardType = "Maestro"
+        val amountTransfersThisMonth = 75_000
+        val amountTransfer = 60_000
+        //act
+        val result = commissionCalculate(cardType, amountTransfersThisMonth, amountTransfer)
+        //assert
+        assertEquals(0, result)
+    }
+
+    @Test
+    fun commissionCalculateVisa() {
+        //arrage
+        val cardType = "Visa"
+        val amountTransfersThisMonth = 75_000
+        val amountTransfer = 60_000
+        //act
+        val result = commissionCalculate(cardType, amountTransfersThisMonth, amountTransfer)
+        //assert
+        assertEquals(45000, result)
+    }
+
+    @Test
+    @Ignore
+    fun limitsCheck() {
+        val cardType = "Maestro"
+        val amountTransfersThisMonth = 75_000
+        val amountTransfer = 60_000
+        //act
+        val result = limitsCheck(cardType, amountTransfersThisMonth, amountTransfer)
+        //assert
+        assertEquals(0, result)
+    }
+}
+
